6a9be9692b3312ace5ce217d86975527addc7748
djordje-devolut <djordje@devolut.io>
2023-12-21 11:05:15 +0100

    Reorganize structure, change file naming

--- create-devolut-aws-user/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-aws iam create-user --user-name devolut-ro
-aws iam put-user-policy --user-name devolut-ro --policy-name DevolutReadOnlyPolicy --policy-document file://policies.json
-aws iam create-access-key --user-name devolut-ro
