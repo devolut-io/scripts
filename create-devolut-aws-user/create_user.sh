c34340bf8238873f549a0f962d1572a94df69db0
djordje-devolut <djordje@devolut.io>
2023-12-21 11:13:43 +0100

    Debug

--- create-devolut-aws-user/create_user.sh
+++ create-devolut-aws-user/create_user.sh
@@ -2,6 +2,8 @@
 
 USERNAME=devolut-ro
 
+pwd
+
 aws iam create-user --user-name $USERNAME
 aws iam create-login-profile --user-name $USERNAME --password devolut@2024 --password-reset-required
 aws iam put-user-policy --user-name $USERNAME --policy-name DevolutReadOnlyPolicy --policy-document file://policies/read_only_all.json
