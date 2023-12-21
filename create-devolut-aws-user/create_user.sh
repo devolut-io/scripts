980270e097c925f969d32ea6c769cdc81bf81d13
djordje-devolut <djordje@devolut.io>
2023-12-21 11:21:21 +0100

    Change default password

--- create-devolut-aws-user/create_user.sh
+++ create-devolut-aws-user/create_user.sh
@@ -6,6 +6,6 @@ pwd
 cat policies/read_only_all.json
 
 aws iam create-user --user-name $USERNAME
-aws iam create-login-profile --user-name $USERNAME --password devolut@2024 --password-reset-required
+aws iam create-login-profile --user-name $USERNAME --password devolut2024 --password-reset-required
 aws iam put-user-policy --user-name $USERNAME --policy-name DevolutReadOnlyPolicy --policy-document file://policies/read_only_all.json
 aws iam create-access-key --user-name $USERNAME
