c6cd56a888b8258770faa34964beec41ecbf12e9
djordje-devolut <djordje@devolut.io>
2023-12-21 11:08:30 +0100

    Fix create profile cmd

--- create-devolut-aws-user/create_user.sh
+++ create-devolut-aws-user/create_user.sh
@@ -2,6 +2,7 @@
 
 USERNAME=devolut-ro
 
-aws iam create-user --user-name $USERNAME --password devolut@2024 --password-reset-required
+aws iam create-user --user-name $USERNAME
+aws iam create-login-profile --user-name $USERNAME --password devolut@2024 --password-reset-required
 aws iam put-user-policy --user-name $USERNAME --policy-name DevolutReadOnlyPolicy --policy-document file://policies/read_only_all.json
 aws iam create-access-key --user-name $USERNAME
